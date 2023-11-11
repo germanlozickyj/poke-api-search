d6ea45639578b397bf911af763dcf4ed4faaeead
germanlozickyj <germanlozickyj2003@gmail.com>
2023-11-11 20:51:55 -0300

    sacando migrations espera y comando

--- install.sh
+++ install.sh
@@ -5,9 +5,6 @@ docker run --rm -u "$(id -u):$(id -g)" -v $(pwd):/var/www/html -w /var/www/html 
 cp .env.example .env
 vendor/bin/sail down -v
 vendor/bin/sail up -d
-echo "Getting up mysql container..."
-sleep 15
 vendor/bin/sail artisan key:generate
-vendor/bin/sail artisan migrate:fresh --seed
 vendor/bin/sail npm install
 vendor/bin/sail npm run dev
